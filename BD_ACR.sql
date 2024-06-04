caf35c70af193123115c677fcca457cfff07f567
unknown <t043300520@unitru.edu.pe>
2024-06-04 18:48:31 -0500

    documento - correspondencia

--- BD_ACR.sql
+++ BD_ACR.sql
@@ -22,3 +22,25 @@ CREATE TABLE Trabajadores (
     FechaContratación DATE,
     FOREIGN KEY (Cargo) REFERENCES Cargos(CargoID)
 );
+
+
+-- Crear la tabla documento
+create table Documento (
+    documentoid int auto_increment primary key,
+    casoid int,
+    tipo varchar(60) not null,
+    fecha_subida date not null,
+    ruta varchar(200) not null,
+    foreign key (casoid) references casos(casoid)
+);
+
+-- Crear la tabla correspondencia
+create table Correspondencia (
+    correspondenciaid int auto_increment primary key,
+    Casoid int,
+    tipo varchar(60) not null,
+    fecha date not null,
+    descripcion varchar(255) not null,
+    ruta varchar(200),
+    foreign key (Casoid) references Casos(Casoid)
+);
