6a6625bf931c4d1b342572c2ecaf492f3e8911a4
unknown <t043300520@unitru.edu.pe>
2024-06-18 15:46:25 -0500

    cargo de trabajador

--- backend-acr-iw-o/src/main/java/com/backend_acr/springboot/web/app/trabajador/TrabajadorService.java
+++ backend-acr-iw-o/src/main/java/com/backend_acr/springboot/web/app/trabajador/TrabajadorService.java
@@ -1,10 +1,12 @@
 package com.backend_acr.springboot.web.app.trabajador;
 
+import com.backend_acr.springboot.web.app.cargo.Cargo;
+import com.backend_acr.springboot.web.app.cargo.CargoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class TrabajadorService {
@@ -12,50 +14,34 @@ public class TrabajadorService {
     @Autowired
     private TrabajadorRepository trabajadorRepository;
 
-    public String addTrabajador(TrabajadorDTO trabajadorDTO) {
-        Trabajador trabajador = new Trabajador();
-        trabajador.setNombres(trabajadorDTO.getNombres());
-        trabajador.setApellidos(trabajadorDTO.getApellidos());
-        trabajador.setDireccion(trabajadorDTO.getDireccion());
-        trabajador.setEmail(trabajadorDTO.getEmail());
-        trabajador.setCelular(trabajadorDTO.getCelular());
-        trabajador = trabajadorRepository.save(trabajador);
-        return String.valueOf(trabajador.getTrabajadorId());
+    @Autowired
+    private CargoRepository cargoRepository;
+
+    public Trabajador addTrabajador(Trabajador trabajador) {
+        return trabajadorRepository.save(trabajador);
     }
 
-    public List<TrabajadorDTO> getAllTrabajadores() {
-        return trabajadorRepository.findAll().stream().map(this::convertToDTO).collect(Collectors.toList());
+    public List<Trabajador> getAllTrabajadores() {
+        return trabajadorRepository.findAll();
     }
 
-    public TrabajadorDTO getTrabajadorById(int id) {
-        Trabajador trabajador = trabajadorRepository.findById(id).orElseThrow(() -> new RuntimeException("Trabajador no encontrado"));
-        return convertToDTO(trabajador);
+    public Optional<Trabajador> getTrabajadorById(int id) {
+        return trabajadorRepository.findById(id);
     }
 
-    public TrabajadorDTO updateTrabajador(int id, TrabajadorDTO trabajadorDTO) {
-        Trabajador trabajador = trabajadorRepository.findById(id).orElseThrow(() -> new RuntimeException("Trabajador no encontrado"));
-        trabajador.setNombres(trabajadorDTO.getNombres());
-        trabajador.setApellidos(trabajadorDTO.getApellidos());
-        trabajador.setDireccion(trabajadorDTO.getDireccion());
-        trabajador.setEmail(trabajadorDTO.getEmail());
-        trabajador.setCelular(trabajadorDTO.getCelular());
-        trabajador = trabajadorRepository.save(trabajador);
-        return convertToDTO(trabajador);
+    public Trabajador updateTrabajador(int id, Trabajador trabajadorDetails) {
+        Trabajador trabajador = trabajadorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Trabajador not found"));
+        trabajador.setNombres(trabajadorDetails.getNombres());
+        trabajador.setApellidos(trabajadorDetails.getApellidos());
+        trabajador.setDireccion(trabajadorDetails.getDireccion());
+        trabajador.setEmail(trabajadorDetails.getEmail());
+        trabajador.setCelular(trabajadorDetails.getCelular());
+        trabajador.setCargo(trabajadorDetails.getCargo());
+        return trabajadorRepository.save(trabajador);
     }
 
     public void deleteTrabajador(int id) {
         trabajadorRepository.deleteById(id);
     }
-
-    private TrabajadorDTO convertToDTO(Trabajador trabajador) {
-        TrabajadorDTO trabajadorDTO = new TrabajadorDTO();
-        trabajadorDTO.setTrabajadorId(trabajador.getTrabajadorId());
-        trabajadorDTO.setNombres(trabajador.getNombres());
-        trabajadorDTO.setApellidos(trabajador.getApellidos());
-        trabajadorDTO.setDireccion(trabajador.getDireccion());
-        trabajadorDTO.setEmail(trabajador.getEmail());
-        trabajadorDTO.setCelular(trabajador.getCelular());
-        return trabajadorDTO;
-    }
 }
-
