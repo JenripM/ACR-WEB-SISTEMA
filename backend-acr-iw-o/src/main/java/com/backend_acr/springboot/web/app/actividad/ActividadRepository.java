95bc210e7ca4c30f2e22e6c3f5cd1c884fcb8108
Andre Prieto <73384545cometa@gmail.com>
2024-06-19 13:39:18 -0500

    Agregado actividad

---
+++ backend-acr-iw-o/src/main/java/com/backend_acr/springboot/web/app/actividad/ActividadRepository.java
@@ -0,0 +1,7 @@
+package com.backend_acr.springboot.web.app.actividad;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActividadRepository extends JpaRepository<Actividad, Integer> {
+
+}
