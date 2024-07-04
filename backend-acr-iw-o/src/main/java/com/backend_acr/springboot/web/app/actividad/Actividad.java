64fec8605dda5f931709d5ae09c0b68fec6d2a9c
Andre Prieto <73384545cometa@gmail.com>
2024-07-04 17:31:14 -0500

    Transicion

--- backend-acr-iw-o/src/main/java/com/backend_acr/springboot/web/app/actividad/Actividad.java
+++ backend-acr-iw-o/src/main/java/com/backend_acr/springboot/web/app/actividad/Actividad.java
@@ -124,13 +124,4 @@ public class Actividad {
     public void setCaso(Caso caso) {
         this.caso = caso;
     }
-
-    public Trabajador getTrabajador() {
-        return trabajador;
-    }
-
-    public void setTrabajador(Trabajador trabajador) {
-        this.trabajador = trabajador;
-    }
-
 }
