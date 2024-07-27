ad5e1178e3ed3679e2e80a209a96f92eab4081b6
unknown <t043300520@unitru.edu.pe>
2024-07-26 22:38:15 -0500

    se implemento chatbot con IA

--- ACR-FRONT/src/app/pages/dashboard/dashboard.component.ts
+++ ACR-FRONT/src/app/pages/dashboard/dashboard.component.ts
@@ -1,418 +1,173 @@
-import { CommonModule } from '@angular/common';
-import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatTableModule } from '@angular/material/table';
-import { TablerIconsModule } from 'angular-tabler-icons';
 import {
-  ApexChart,
-  ChartComponent,
-  ApexDataLabels,
-  ApexLegend,
-  ApexStroke,
-  ApexTooltip,
-  ApexAxisChartSeries,
-  ApexXAxis,
-  ApexYAxis,
-  ApexGrid,
-  ApexPlotOptions,
-  ApexFill,
-  ApexMarkers,
-  ApexResponsive,
-  NgApexchartsModule,
-} from 'ng-apexcharts';
-
-interface month {
-  value: string;
-  viewValue: string;
-}
-
-export interface profitExpanceChart {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  dataLabels: ApexDataLabels;
-  plotOptions: ApexPlotOptions;
-  yaxis: ApexYAxis;
-  xaxis: ApexXAxis;
-  fill: ApexFill;
-  tooltip: ApexTooltip;
-  stroke: ApexStroke;
-  legend: ApexLegend;
-  grid: ApexGrid;
-  marker: ApexMarkers;
-}
-
-export interface trafficChart {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  dataLabels: ApexDataLabels;
-  plotOptions: ApexPlotOptions;
-  tooltip: ApexTooltip;
-  stroke: ApexStroke;
-  legend: ApexLegend;
-  responsive: ApexResponsive;
-}
-
-export interface salesChart {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  dataLabels: ApexDataLabels;
-  plotOptions: ApexPlotOptions;
-  tooltip: ApexTooltip;
-  stroke: ApexStroke;
-  legend: ApexLegend;
-  responsive: ApexResponsive;
-}
-
-interface stats {
-  id: number;
-  time: string;
-  color: string;
-  title?: string;
-  subtext?: string;
-  link?: string;
-}
-
-export interface productsData {
-  id: number;
-  imagePath: string;
-  uname: string;
-  position: string;
-  hourRate: number;
-  classes: number;
-  priority: string;
-}
-
-// ecommerce card
-interface productcards {
-  id: number;
-  imgSrc: string;
-  title: string;
-  price: string;
-  rprice: string;
-}
-
-const ELEMENT_DATA: productsData[] = [
-  {
-    id: 1,
-    imagePath: 'assets/images/profile/user-1.jpg',
-    uname: 'Mark J. Freeman',
-    position: 'English',
-    hourRate: 150,
-    classes: 53,
-    priority: 'Available',
-  },
-  {
-    id: 2,
-    imagePath: 'assets/images/profile/user-2.jpg',
-    uname: 'Andrew McDownland',
-    position: 'Project Manager',
-    hourRate: 150,
-    classes: 68,
-    priority: 'In Class',
-  },
-  {
-    id: 3,
-    imagePath: 'assets/images/profile/user-3.jpg',
-    uname: 'Christopher Jamil',
-    position: 'Project Manager',
-    hourRate: 150,
-    classes: 94,
-    priority: 'Absent',
-  },
-  {
-    id: 4,
-    imagePath: 'assets/images/profile/user-4.jpg',
-    uname: 'Nirav Joshi',
-    position: 'Frontend Engineer',
-    hourRate: 150,
-    classes: 27,
-    priority: 'On Leave',
-  },
-];
-
+  Component,
+  ElementRef,
+  TemplateRef,
+  ViewChild,
+  inject,
+} from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NgxLoadingModule, ngxLoadingAnimationTypes } from "ngx-loading";
+import { trigger, style, transition, animate } from "@angular/animations";
+import { DataService } from "../../data.service";
+import { CommonModule } from "@angular/common";
+import { ConvertTextToHtmlPipe } from "../../convert-text-to-html.pipe";
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from "@angular/forms";
+import { GeminiConfig } from "../../chat-form";
+import { API_KEY_CONF } from "../../../config";
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSelectModule } from "@angular/material/select";
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  encapsulation: ViewEncapsulation.None,
+  selector: "app-dashboard",
   standalone: true,
   imports: [
-    MatButtonModule,
-    MatMenuModule,
-    MatIconModule,
-    TablerIconsModule,
-    MatCardModule,
-    NgApexchartsModule,
-    MatTableModule,
+    RouterOutlet,
     CommonModule,
+    NgxLoadingModule,
+    ConvertTextToHtmlPipe,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatSelectModule
+  ],
+  templateUrl: "./dashboard.component.html",
+  styleUrl: "./dashboard.component.scss",
+  animations: [
+    trigger("typeWritterEffect", [
+      transition(":enter", [
+        style({ opacity: 0 }),
+        animate("2s", style({ opacity: 1 })),
+      ]),
+    ]),
   ],
 })
 export class AppDashboardComponent {
-  @ViewChild('chart') chart: ChartComponent = Object.create(null);
-
-  public profitExpanceChart!: Partial<profitExpanceChart> | any;
-  public trafficChart!: Partial<trafficChart> | any;
-  public salesChart!: Partial<salesChart> | any;
-
-  displayedColumns: string[] = ['profile', 'hrate', 'exclasses', 'status'];
-  dataSource = ELEMENT_DATA;
-
-  months: month[] = [
-    { value: 'mar', viewValue: 'March 2023' },
-    { value: 'apr', viewValue: 'April 2023' },
-    { value: 'june', viewValue: 'June 2023' },
+  @ViewChild("messagesContainer") private messagesContainer!: ElementRef;
+  private dataService = inject(DataService);
+  public messagesHistory: { role: string; parts: string }[] = [];
+  public userMessage!: string | null;
+  public loading = false;
+  public loadingTemplate!: TemplateRef<any>;
+  public ngxLoadingAnimationTypes = ngxLoadingAnimationTypes;
+  public loadingConfig = {
+    animationType: ngxLoadingAnimationTypes.circleSwish,
+    primaryColour: "#ffffff",
+    secondaryColour: "#ccc",
+    tertiaryColour: "#dd0031",
+    backdropBorderRadius: "3px",
+  };
+  public gQuestions = [
+    
+  ];
+  public bQuestions = [
+   
   ];
 
-  // recent transaction
-  stats: stats[] = [
+  public characterSelection = [
+    {
+      id: 0,
+      value: "Rosales y Asociados",
+    },
     {
       id: 1,
-      time: '09.30 am',
-      color: 'primary',
-      subtext: 'Payment received from John Doe of $385.90',
-    },
-    {
-      id: 2,
-      time: '10.30 am',
-      color: 'accent',
-      title: 'New sale recorded',
-      link: '#ML-3467',
-    },
-    {
-      id: 3,
-      time: '12.30 pm',
-      color: 'success',
-      subtext: 'Payment was made of $64.95 to Michael',
-    },
-    {
-      id: 4,
-      time: '12.30 pm',
-      color: 'warning',
-      title: 'New sale recorded',
-      link: '#ML-3467',
-    },
-    {
-      id: 5,
-      time: '12.30 pm',
-      color: 'error',
-      title: 'New arrival recorded',
-      link: '#ML-3467',
-    },
-    {
-      id: 6,
-      time: '12.30 pm',
-      color: 'success',
-      subtext: 'Payment Done',
+      value: "Gemini",
     },
   ];
 
-  // ecommerce card
-  productcards: productcards[] = [
+  temperatureOptions = [
+    { value: 0.2, label: "Low Creativity" },
+    { value: 0.5, label: "Moderate Creativity" },
+    { value: 0.9, label: "High Creativity" },
+  ];
+
+  modelOptions = [
+    { label: "Gemini v1.0.0-Pro (Basic)", value: "gemini-1.0-pro" },
+    { label: "Gemini v1.0.0-Pro-001 (Updated)", value: "gemini-1.0-pro-001" },
     {
-      id: 1,
-      imgSrc: '/assets/images/products/p1.jpg',
-      title: 'Boat Headphone',
-      price: '285',
-      rprice: '375',
-    },
-    {
-      id: 2,
-      imgSrc: '/assets/images/products/p2.jpg',
-      title: 'MacBook Air Pro',
-      price: '285',
-      rprice: '375',
-    },
-    {
-      id: 3,
-      imgSrc: '/assets/images/products/p3.jpg',
-      title: 'Red Valvet Dress',
-      price: '285',
-      rprice: '375',
-    },
-    {
-      id: 4,
-      imgSrc: '/assets/images/products/p4.jpg',
-      title: 'Cute Soft Teddybear',
-      price: '285',
-      rprice: '375',
+      label: "Gemini v1.5 (Experimental)",
+      value: "gemini-1.5-pro",
+      disabled: true,
     },
   ];
 
-  constructor() {
-    // sales overview chart
-    this.profitExpanceChart = {
-      series: [
-        {
-          name: 'Eanings this month',
-          data: [9, 5, 3, 7, 5, 10, 3],
-          color: '#0085db',
-        },
-        {
-          name: 'Expense this month',
-          data: [6, 3, 9, 5, 4, 6, 4],
-          color: '#fb977d',
-        },
-      ],
+  chatForm = new FormGroup({
+    apiKey: new FormControl(API_KEY_CONF || ""),
+    temperature: new FormControl(this.temperatureOptions[2].value),
+    bot: new FormControl(this.characterSelection[0]),
+    model: new FormControl(this.modelOptions[0].value),
+  });
 
-      grid: {
-        borderColor: 'rgba(0,0,0,0.1)',
-        strokeDashArray: 3,
+  sendMessage(message: string) {
+    if (!message || this.loading) return;
+    setTimeout(() => this.scrollToBottom(), 0);
+    this.loading = true;
+    this.messagesHistory.push(
+      {
+        role: "user",
+        parts: message,
       },
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          columnWidth: '30%',
-          borderRadius: 4,
-          endingShape: "rounded",
-        },
-      },
-      chart: {
-        type: 'bar',
-        height: 390,
-        offsetY: 10,
-        foreColor: '#adb0bb',
-        fontFamily: 'inherit',
-        toolbar: { show: false },
-      },
-      dataLabels: { enabled: false },
-      markers: { size: 0 },
-      legend: { show: false },
-      xaxis: {
-        type: 'category',
-        categories: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        axisTicks: {
-          show: false,
-        },
-        axisBorder: {
-          show: false,
-        },
-        labels: {
-          style: { cssClass: 'grey--text lighten-2--text fill-color' },
-        },
-      },
-      stroke: {
-        show: true,
-        width: 5,
-        colors: ['transparent'],
-      },
-      tooltip: { theme: 'light' },
-
-      responsive: [
-        {
-          breakpoint: 600,
-          options: {
-            plotOptions: {
-              bar: {
-                borderRadius: 3,
-              },
+      {
+        role: "model",
+        parts: "",
+      }
+    );
+    this.dataService
+      .generateContentWithGeminiPro(
+        message,
+        this.messagesHistory,
+        this.chatForm.value as GeminiConfig
+      )
+      .subscribe({
+        next: (res: any) => {
+          this.loading = false;
+          this.userMessage = null;
+          this.messagesHistory = this.messagesHistory.slice(0, -2);
+          this.messagesHistory.push(
+            {
+              role: "user",
+              parts: message,
             },
-          },
+            {
+              role: "model",
+              parts: res,
+            }
+          );
+          setTimeout(() => this.scrollToBottom(), 0);
         },
-      ],
-    };
+        error: (error) => {
+          this.loading = false;
+          console.error("Error generating content:", error);
+          this.messagesHistory.push({
+            role: "model",
+            parts: "Lo Siento, Intenta nuevamente.",
+          });
+          setTimeout(() => this.scrollToBottom(), 0);
+        },
+      });
+  }
 
-    // yearly breakup chart
-    this.trafficChart = {
-      series: [5368, 3500, 4106],
-      labels: ['5368', 'Refferal Traffic', 'Oragnic Traffic'],
-      chart: {
-        type: 'donut',
-        fontFamily: "'Plus Jakarta Sans', sans-serif;",
-        foreColor: '#adb0bb',
-        toolbar: {
-          show: false,
-        },
-        height: 160,
-      },
-      colors: ['#e7ecf0', '#fb977d', '#0085db'],
-      plotOptions: {
-        pie: {
-          donut: {
-            size: '80%',
-            background: 'none',
-            labels: {
-              show: true,
-              name: {
-                show: true,
-                fontSize: '12px',
-                color: undefined,
-                offsetY: 5,
-              },
-              value: {
-                show: false,
-                color: '#98aab4',
-              },
-            },
-          },
-        },
-      },
-      stroke: {
-        show: false,
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      legend: {
-        show: false,
-      },
-      responsive: [
-        {
-          breakpoint: 991,
-          options: {
-            chart: {
-              width: 120,
-            },
-          },
-        },
-      ],
-      tooltip: {
-        enabled: false,
-      },
-    };
+  scrollToBottom(): void {
+    try {
+      this.messagesContainer.nativeElement.scrollTop =
+        this.messagesContainer.nativeElement.scrollHeight;
+    } catch (err) {}
+  }
 
-    // mohtly earnings chart
-    this.salesChart = {
-      series: [
-        {
-          name: '',
-          color: '#8763da',
-          data: [25, 66, 20, 40, 12, 58, 20],
-        },
-      ],
-
-      chart: {
-        type: 'area',
-        fontFamily: "'Plus Jakarta Sans', sans-serif;",
-        foreColor: '#adb0bb',
-        toolbar: {
-          show: false,
-        },
-        height: 60,
-        sparkline: {
-          enabled: true,
-        },
-        group: 'sparklines',
-      },
-      stroke: {
-        curve: 'smooth',
-        width: 2,
-      },
-      fill: {
-        colors: ['#8763da'],
-        type: 'solid',
-        opacity: 0.05,
-      },
-      markers: {
-        size: 0,
-      },
-      tooltip: {
-        theme: 'dark',
-        x: {
-          show: false,
-        },
-      },
-    };
+  ngAfterViewInit() {
+    this.scrollToBottom();
   }
 }
